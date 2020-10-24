8fd223caf611a5ca33b5715029601de6320289e7
jpassgo <jeffpascoe9@gmail.com>
2020-10-24 14:37:30 -0500

    Registering model class and adding app to installed apps.

--- UsersRestApi/models.py
+++ UsersRestApi/models.py
@@ -5,13 +5,7 @@ class User(models.Model):
     name = models.TextField()
     email = models.TextField()
     age = models.IntegerField()
-    id = models.AutoField()
     
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_added=True)
-
-    class Meta:
-        ordering = ['created']
-
+    
     def __unicode__(self):
         return self.name + ' - ' + self.email + ' - ' + self.age + ' - ' + self.id 
