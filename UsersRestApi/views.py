28657c19ff5d0127d4f1d1374c155d488dc68e52
jpassgo <jeffpascoe9@gmail.com>
2020-10-24 14:37:55 -0500

    Grabbing data off request body and creating new user.

--- UsersRestApi/views.py
+++ UsersRestApi/views.py
@@ -1,4 +1,5 @@
 import json
+from .models import User
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from django.views.decorators.csrf import csrf_exempt
@@ -7,7 +8,13 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def user(request):
     if request.method == 'POST':
+        body = json.loads(request.body)
         # create new user and add to mongodb
+        name = body['name']
+        email = body['email']
+        age = body['age']
+
+        User(name, email, age)
         return HttpResponse(
             json.dumps({'request-type': request.method}),
             content_type="application/json")
