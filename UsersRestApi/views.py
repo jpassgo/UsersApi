02e9d2cd763d0305ebf467716d52dc30e67bcb3c
jpassgo <jeffpascoe9@gmail.com>
2020-10-27 20:23:32 -0500

    Cleaning up views

--- UsersRestApi/views.py
+++ UsersRestApi/views.py
@@ -13,14 +13,9 @@ def user(request):
     if request.method == 'POST':
         user_data = JSONParser().parse(request)
         user_serializer = UserSerializer(data=user_data)
+        user_serializer.is_valid()
         user_serializer.save()
-        body = json.loads(request.body)
-        # create new user and add to mongodb
-        name = body['name']
-        email = body['email']
-        age = body['age']
-
-        user = User(name, email, age)
+  
         return JsonResponse(tutorial_serializer.data, status=status.HTTP_201_CREATED) 
 
     elif request.method == 'GET':
