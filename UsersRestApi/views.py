50c4722cfefd29c83ad77563f3e74725d5f0e44f
jpassgo <jeffpascoe9@gmail.com>
2020-11-11 18:59:50 -0600

    Adding logic to get a user from mongodb.

--- UsersRestApi/views.py
+++ UsersRestApi/views.py
@@ -9,10 +9,9 @@ from pymongo import MongoClient
 import json
 
 
-
 @csrf_exempt
-def user(request):
-
+def user(request, id):
+    print("herererer")
     if request.method == 'POST':
         user_data = JSONParser().parse(request)
         user_id = insert(user_data)
@@ -21,9 +20,11 @@ def user(request):
             content_type="application/json"
         )
     elif request.method == 'GET':
-        # attempt to get the user with given id from mongodb
+        id = request.GET.get('id')
+        user = retrieve(id)
+        print(user)
         return HttpResponse(
-            json.dumps({'request-type': request.method}),
+            json.dumps(user),
             content_type="application/json"
         )
     elif request.method == 'DELETE':
@@ -33,14 +34,23 @@ def user(request):
             content_type="application/json"
         )
 
+
 def create_mongo_connection():
     return MongoClient('mongodb://localhost:27017')
+
 
 def get_users_table(client):
     db = client['users_db']
     return db.users
 
+
 def insert(user):
     client = create_mongo_connection()
     users_table = get_users_table(client)
     return users_table.insert_one(user).inserted_id
+
+
+def retrieve(id):
+    client = create_mongo_connection()
+    users_table = get_users_table(client)
+    return users_table.find_one({'id': id})
