7db1fa2dc87e645fb17473a48b4278df8e6e1ce2
jpassgo <jeffpascoe9@gmail.com>
2020-11-15 16:28:36 -0600

    Creating methods for updating values in mongo

--- UsersRestApi/mongodb_interface.py
+++ UsersRestApi/mongodb_interface.py
@@ -1,4 +1,6 @@
+from rest_framework.parsers import JSONParser
 from pymongo import MongoClient
+
 
 
 def create_mongo_connection():
@@ -24,7 +26,18 @@ def retrieve(id):
     return user
 
 
+def update(id, request):
+    client = create_mongo_connection()
+    users_table = get_users_table(client)
+    return users_table.update_one(id, new_values)
+
+
 def delete(id):
     client = create_mongo_connection()
     users_table = get_users_table(client)
     users_table.delete_one({'id': id})
+
+def determine_value_to_update(request):
+    update_request = JSONParser().parse(request)
+    # get the values to be updated
+    update_request
